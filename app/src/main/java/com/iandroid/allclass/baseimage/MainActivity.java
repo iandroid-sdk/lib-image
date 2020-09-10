c8a89ce7adae55a649155e17597a53329ad33f44
kunming.wang <461133899@qq.com>
2020-09-10 21:19:22 +0800

    [FEATURE #00]del uselesscode

--- app/src/main/java/com/iandroid/allclass/baseimage/MainActivity.java
+++ app/src/main/java/com/iandroid/allclass/baseimage/MainActivity.java
@@ -1,11 +1,8 @@
 package com.iandroid.allclass.baseimage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.iandroid.allclass.lib_baseimage.FrescoConfig;
-import com.iandroid.allclass.lib_baseimage.ImageLoader;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String FOLDER_FRESCO_CACHE = "Fresco";
@@ -13,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageLoader.init(this, new FrescoConfig.Builder()
+       /* ImageLoader.init(this, new FrescoConfig.Builder()
                 .localDir("")
-                .build());
+                .build());*/
     }
 }
