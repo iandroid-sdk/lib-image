5e02986f8971f053e0a4f5754ae3df9b4ba93a92
kunming.wang <461133899@qq.com>
2020-09-17 11:41:53 +0800

    [FEATURE #00]webp动图支持

--- app/src/main/java/com/iandroid/allclass/baseimage/MainActivity.java
+++ app/src/main/java/com/iandroid/allclass/baseimage/MainActivity.java
@@ -1,22 +1,25 @@
 package com.iandroid.allclass.baseimage;
 
-import android.net.Uri;
 import android.os.Bundle;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.iandroid.allclass.lib_baseimage.FrescoConfig;
+import com.iandroid.allclass.lib_baseimage.ImageLoader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String FOLDER_FRESCO_CACHE = "Fresco";
+    SimpleDraweeView vv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Uri uri = new Uri.Builder().scheme("res")
-                .authority(getPackageName())
-                .appendPath(String.valueOf(R.drawable.ic_launcher_background))
-                .build();
-       /* ImageLoader.init(this, new FrescoConfig.Builder()
+        ImageLoader.init(this, new FrescoConfig.Builder()
                 .localDir("")
-                .build());*/
+                .build());
+        setContentView(R.layout.activity_main);
+        vv = findViewById(R.id.my_image_view);
+        ImageLoader.displayImage(vv, R.drawable.ic_radio_spectrum);
+
     }
 }
